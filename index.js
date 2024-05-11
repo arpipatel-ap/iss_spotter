dcec1db9692c46260e3948bf98f4dc10071b3568
arpipatel-ap <arpi96@gmail.com>
2024-05-11 15:18:23 -0400

    Add function nextISSTimesForMyLocation

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const { fetchMyIP } = require('./iss');
 const {fetchCoordsByIP} = require('./iss');
 const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+ 
+  printPassTimes(passTimes);
+});
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -12,8 +31,6 @@ fetchMyIP((error, ip) => {
 });
 
 
-
-
 fetchCoordsByIP('72.141.171.121', (error, data) => {
   if (error) {
     console.log("It did not work: ERROR! ", error);
@@ -22,8 +39,6 @@ fetchCoordsByIP('72.141.171.121', (error, data) => {
   console.log('It worked! Returned data:' , data);
 
 });
-
-
 
 
 fetchISSFlyOverTimes(`{ latitude: '49.27670', longitude: '-123.13000' }`, (error, passTimes) => {
