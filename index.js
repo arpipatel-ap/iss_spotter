3b218c304da59a0dc8ff30645d50a7876c1a8765
arpipatel-ap <arpi96@gmail.com>
2024-05-11 12:08:33 -0400

    Add function fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-//const { fetchMyIP } = require('./iss');
+const { fetchMyIP } = require('./iss');
+const {fetchCoordsByIP} =require('./iss')
 
 fetchMyIP((error, ip) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It did not work: ERROR!" , error);
     return;
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+
+
+
+fetchCoordsByIP('72.141.171.121', (error, data) => {
+  if (error) {
+    console.log("It did not work: ERROR! ", error);
+    return;
+  } 
+  console.log('It worked! Returned data:' , data);
+
 });
