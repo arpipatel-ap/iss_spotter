730873a36e768f842084180529ea89c6b7e55bc2
arpipatel-ap <arpi96@gmail.com>
2024-05-11 14:05:50 -0400

    Add function fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { fetchMyIP } = require('./iss');
-const {fetchCoordsByIP} =require('./iss')
+const {fetchCoordsByIP} = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -17,7 +18,19 @@ fetchCoordsByIP('72.141.171.121', (error, data) => {
   if (error) {
     console.log("It did not work: ERROR! ", error);
     return;
-  } 
+  }
   console.log('It worked! Returned data:' , data);
 
+});
+
+
+
+
+fetchISSFlyOverTimes(`{ latitude: '49.27670', longitude: '-123.13000' }`, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
